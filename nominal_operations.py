ef36faa49ab860c3f045837ed23af54da3cfa072
Benjamin-Ponty <benjamin@thespringinstitute.com>
2024-07-11 21:31:21 +0200

    Update nominal_operations.py

--- nominal_operations.py
+++ nominal_operations.py
@@ -11,14 +11,20 @@ class NominalOperations:
         self.control_led_panels()
         # Any actions ? Updates ? 
     
-    def monitor_obc_status(self):
+def monitor_obc_status(self):
         # Monitor temperature, current, etc.
         obc_temp = self.get_obc_temperature()
         obc_current = self.get_obc_current()
         # Tests here Temp high or low ? Critical temperature shutting down some services ? 
-        # Error handling ? 
+        if obc_temp> 100 :
+            self.logger.log_info("nominal_operations",f"OBC Temp: {obc_temp} temperature is too high critical error, critical OBC shutdown\nOBC Current: {obc_current}")
+            po.PowerManager(critical_shutdown=True)
+        if obc_current< 12132 or obc_current>894598:#see which reel value to replace this fake one
+            self.logger.log_info("nominal_operations",f"OBC Current: {obc_current} temperature is too high critical error, critical OBC shutdown\nOBC Temp: {obc_temp}")
+            po.PowerManager(critical_shutdown=True)
         # .....
-        self.logger.log_info(f"OBC Temp: {obc_temp}, OBC Current: {obc_current}")
+        self.logger.log_info("nominal_operations",f"OBC Temp: {obc_temp}, OBC Current: {obc_current}")
+    
     
     def control_led_panels(self):
         # Control LED panels
