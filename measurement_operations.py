50a1d7f5342c2176e89f13067e64bf76f87c2f86
BenjaminBFP <166150574+BenjaminBFP@users.noreply.github.com>
2024-08-04 12:28:06 +0200

    Update measurement_operations.py

--- measurement_operations.py
+++ measurement_operations.py
@@ -1,22 +1,41 @@
 import time
-from utils import Logger, SystemStatus
-
+from Utils import Logger, SystemStatus
+import sys
+sys.path.insert(1,"/home/pi/SCAMPI/Sensors")
+from readT_P import T_P_probe
+from camera import Camera
+from temperature import Temperature
 class MeasurementManager:
     def __init__(self):
-        self.logger = Logger()
+        self.log = Logger()
+        self.camera = Camera()
+        self.tempressure= T_P_probe()
+        self.temp = Temperature()
 
-def perform_measurements(self):
-    self.logger.log_info("Performing measurements")
-    self.measure_abiotic_factors()
-    self.record_videos_and_photos()
-    # ..... Is something missing in the sequence ? The probe perhaps ? :) 
+    def perform_sensors_measurements(self):
+        self.log.log_info("measurement_operation","Performing measurements")
+        self.measure_external_temp()
+        self.measure_abiotic_factors(17)
+        self.record_photos()
+        self.record_videos(True) #When starting take a long video
+        
+    def measure_external_temp(self):
+        self.temp.get_temperature()
+        
 
-def measure_abiotic_factors(self):
-    # Measure temperature, pressure, etc.
-    # ......
-     self.sensor_input = 5 
-"""Fake input to emulate the temperature reading"""
-def record_videos_and_photos(self):
-    print("")
-    # Record videos and take photos
-    # ........
+    def measure_abiotic_factors(self,pin):
+        #self.tempressure.readProbe("<pin number>") 
+        self.tempressure.readProbe(17)
+        #self.pressure = self.tempressure.pressure
+	    #self.inside_temp = self.tempressure.inside_temp  uncomment when sensors are connected
+        
+    """Fake input to emulate the temperature reading"""
+    def record_photos(self):
+        self.camera.capture_photo()
+        # Record videos and take photos
+        # ........
+    def record_videos(self,Long=False):
+        self.camera.capture_video(Long)
+if __name__ == "__main__":
+    test = MeasurementManager()
+    test.measure_external_temp()
