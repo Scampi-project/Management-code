efa081ba49f76478651a2ad296bdf029a19b1722
BenjaminBFP <166150574+BenjaminBFP@users.noreply.github.com>
2025-01-21 22:58:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,45 +12,36 @@ class Main:
     def __init__(self):
         # Initialize all modules
         self.power_manager = PowerManager()
-        self.nominal_ops = NominalOperations()
         self.measure = MeasurementManager()
+        self.nominal_ops = NominalOperations()
         self.systate = SystemStatus()
+        self.log = Logger()
         # data_analyzer = DataAnalyzer()
         # transmission_manager = TransmissionManager()
-        self.log = Logger()
-
-
-        self.systate.update_mode('PowerOn')
-        self.power_manager.check_power_status()
-        self.systate.set_mode('PowerOn') 
-        self.systate.update_mode('Initialization')
-        self.power_manager.handle_power_on()
-
-        self.systate.set_mode('Initialization',self.power_manager.critical)
-        self.systate.update_mode('Nominal')
-        self.nominal_ops.perform_nominal_operations()
-        self.measure.perform_sensors_measurements()
-        self.time = time.time()
+        self.power_manager.run_initialization_sequence(self.measure)
     def main_loop(self) :
-        self.count = 1
+        self.count = 0
         # Main loop
         while True:
             try:
                 self.count +=1
-                print("5 minutes se sont écoulées")
                 # Check and handle power status
                 self.power_manager.check_power_status()
                 # Perform nominal operations
                  # OBC condition, waiting for 5min
                 self.nominal_ops.perform_nominal_operations()
                 if self.count%6 == 0: # temp & pressure condition, waiting for 30min
+                    print("30 minutes se sont écoulées, OBC")
                     self.measure.perform_sensors_measurements()
                 if self.count%12 == 0 : # short photo condition, waiting for 1h
+                    print("1h minutes s'est écoulée, photo")
                     self.measure.record_photos()
-                if self.count%36 == 0: # short video condition, waiting for 30h
+                if self.count%360 == 0: # short video condition, waiting for 30h
                     self.measure.record_videos()    
+                    print("30h se sont écoulées, short video")
                 if self.count%1440 == 0: # long video condition, waiting for 5days
                     self.measure.record_videos(True)    
+                    print("5jours se sont écoulées, longue video")
                     
                 # Perform measurements if in Measurement mode
                 # if self.systate.current_mode == 'Measurement':
@@ -63,7 +54,7 @@ class Main:
                 # if self.systate.current_mode == 'Transmission':
                 #     transmission_manager.transmit_data()
                 #waiting 5 min for recording new data 
-                time.sleep(300.0)   
+                time.sleep(300.0)   #change the  time for the real condition test 
             except Exception as e:
                 self.log.log_info("errors",f"Error in main loop: {str(e)}")
                 # You need to expand on the error with the ones that are easy to handle.
@@ -75,15 +66,18 @@ class Main:
     def bouton2(self):
         self.measure.record_videos(True)
     def bouton3(self):
-        self.systate.update_mode("Nominal")
+        self.systate.update_mode(systate.current_mode,"Nominal")
     def bouton4(self):
-        self.systate.update_mode("Transmission")
+        self.systate.update_mode(current_mode,"Transmission")
     def bouton5(self):
-        self.systate.update_mode("Data_analysis")
+        self.systate.update_mode(current_mode,"Data_analysis")
+    def bouton6(self):
+        self.power_manager.shutdown_sequence()
         
     def menu(self):
+        print("je suis là")
         window = tkinter.Tk()
-        window.geometry("800x800")
+        window.geometry("800x500")
         window.title("Command Menu")
         #CAMERA PART
         cam_lab = tkinter.Label (text = "Camera management")
@@ -103,39 +97,17 @@ class Main:
         button3.place(x=460,y=160)
         button4.place(x=560,y=160)
         button5.place(x=560,y=190)
-        # SCHEDULED PART 
-        scheduled = tkinter.Label (text = "Scheduled Power Off")
-        from_ = tkinter.Label (text = "from")
-        to = tkinter.Label (text = "to")
-        
-        start = tkinter.Entry(width=10)
-        start.insert(0,"11/10/2004")
-        
-        start_heure = tkinter.Entry(width=5)   
-        start_heure.insert(0,"11h11")
-        
-        end = tkinter.Entry(width=10)
-        end.insert(0,"19/10/2004")
-        
-        end_heure = tkinter.Entry(width=5)  
-        end_heure.insert(0,"11h11")   
-          
-        
-        scheduled.place(x=100,y=300)
-        start.place(x=60,y=360)
-        from_.place(x=20,y=360)
-        to.place(x=155, y= 360)
-        end.place(x=180,y=360)
-        start_heure.place(x=80,y=390)
-        end_heure.place(x=200,y=390)
-        self.power_manager.handle_scheduled_shutdown(start.get(),start_heure.get(),end.get(),end_heure.get())
-        window.mainloop() 
+        shutdown = tkinter.Label (text = "Shutdown")
+        button6 = tkinter.Button(text = "Shutdown",command=self.bouton3)
+        shutdown.place(x=500,y=240)
+        button6.place(x=490,y=270)
+        window.mainloop()
 
 if __name__ == "__main__":
     test= Main()
     thread1 = threading.Thread(target=test.main_loop)
     thread2 = threading.Thread(target=test.menu)
-
-    thread1.start()
     thread2.start()
+    thread1.start()
+    
  
